dd157dfe32144771791d327367b0e3cf5b98fafa
Mohd Haider <mohdhaider1811@gmail.com>
2019-11-27 01:18:49 +0530

    Lacture 25 finished.

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ const navigator = createStackNavigator(
     List: ListScreen
   },
   {
-    initialRouteName: 'List',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'List Screen'
+      title: 'Home Screen'
     }
   }
 );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.textStyle}>Hello There</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>Hi There!</Text>
+      <Button
+        title = "Go to Components Demo"
+        onPress = {() => console.log('Components button pressed')}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
