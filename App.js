5cc67f1ec8358fe320faa8b6ec716c22b14bd330
Mohd Haider <mohdhaider1811@gmail.com>
2019-12-13 01:27:14 +0530

    Shown Image Screen. Initial setup. Working on that.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen'
 import ListScreen from './src/screens/ListScreen'
+import ImageScreen from './src/screens/ImageScreen'
 
 const navigator = createStackNavigator(
   {
